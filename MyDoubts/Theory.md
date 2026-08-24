76c0f30138a7dcee24d90aea67876b25a63f8caf
Samrdhi-SinGh <singhsamridhi878@gmail.com>
2026-08-24 16:57:15 +0530

    /make Add to cart button interactive

--- MyDoubts/Theory.md
+++ MyDoubts/Theory.md
@@ -6,3 +6,15 @@ Like; there is 10.9 and we want 10.90. So we do
 
 Benefit of Generating the HTML in JS:
 If we want to add new product in the page, we don't have to rewrite all the html code, we just have to add ṭhe product in array in js and it will be generated.
+
+Data Attribute: is another HTML attribute which allows us to attach any information to an element.
+
+Syntax of data attribute:
+- is just an HTML attribute.
+- have to start with "data-" always
+- then give it any name.
+
+Like: data-product-name = "${product.name}"
+         name(kebab case)      v alue
+
+Element.dataset property: Gives all the data attribute attached to the element.
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -45,14 +45,42 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+          data-product-id = "${product.id}">
             Add to Cart
           </button>
         </div>
   `;
 });
 
-console.log(productsHTML);
+
 
 document.querySelector('.js-products-grid').
   innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').
+  forEach((button) => {
+    button.addEventListener('click', () => {
+    const productId= button.dataset.productId;
+    console.log(productId);
+
+      let matchingItem;
+
+      cart.forEach((item) => {//item will contain productName and quantity.
+        if (productId === item.productId) {
+          matchingItem = item;
+        }
+      });
+      if (matchingItem) {//Truthy
+        matchingItem.quantity += 1;
+      } else {
+        cart.push({
+          productId: productId,
+          quantity: 1
+        });
+      }
+
+      console.log(cart);
+    });
+  });
+
