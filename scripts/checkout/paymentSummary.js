d31a66037e79d5bcc8cbedcee981c0ca856ff28d
Samrdhi-SinGh <singhsamridhi878@gmail.com>
2026-09-01 17:30:57 +0530

    Create order using backend

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { cart } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
 import { formatCurrency } from '../utils/money.js'
+import { addOrder } from "../../data/order.js";
 
 export function renderPaymentSummary() {
   let productPriceCents = 0;
@@ -21,7 +22,7 @@ export function renderPaymentSummary() {
   const totalCents = totalBeforeTaxCents + taxCents;
 
   let cartQuantity = 0;
-  cart.forEach((cartItem)=>{
+  cart.forEach((cartItem) => {
     cartQuantity += cartItem.quantity;
   })
 
@@ -55,11 +56,34 @@ export function renderPaymentSummary() {
           <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
         </div>
 
-        <button class="place-order-button button-primary">
+        <button class="place-order-button button-primary
+        js-place-order">
           Place your order
         </button>
   `;
 
   document.querySelector('.js-payment-summary')
-  .innerHTML = paymentSummaryHTML;
+    .innerHTML = paymentSummaryHTML;
+
+  document.querySelector('.js-place-order')
+    .addEventListener('click', async () => {
+      try {
+        const response = await fetch('https://supersimplebackend.dev/orders', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            cart: cart
+          })
+        })
+
+        const order = await response.json();
+        addOrder(order);
+      } catch (error) {
+        console.log('Unexpected error. Try again later.')
+      }
+
+      window.location.href = 'orders.html';//Its a filepath, current path is still checkout.html
+    });
 }
